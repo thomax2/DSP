1f23f1b43f2d10776a2d6535c1bffdbd37399022
thomax <yangkaidi1525@163.com>
2024-07-20 15:38:13 +0800

    PCA complete

--- PCA/image_compress.m
+++ PCA/image_compress.m
@@ -1,10 +1,17 @@
 clear;
 clc;
 
-% 假设图像文件名为 'image.jpg'，并且它位于当前工作目录中
-colorImage = imread('pic2.png');
- 
-grayImage = rgb2gray(colorImage);
- 
-% % imshow(grayImage);
-grayImage=double(grayImage);
+% % 假设图像文件名为 'image.jpg'，并且它位于当前工作目录中
+% colorImage = imread('pic2.png');
+%  
+% grayImage = rgb2gray(colorImage);
+%  
+% % % imshow(grayImage);
+% grayImage=double(grayImage);
+
+
+A = [1,2,3,4,5;6,7,8,9,10;11,12,13,14,14];
+
+Y = PCA(A,2);
+
+[coeff,score,latent,tsquared,explained,mu] = pca(A);
